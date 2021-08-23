f99590eaaee17c8a5cd214435a1f52d37589827d
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-23 21:15:00 +0200

    [add] getRandomComics function

--- src/components/Comics.js
+++ src/components/Comics.js
@@ -2,4 +2,14 @@ import React from 'react';
 
 export default function Comics() {
     
+    const getRandomComics = () => {
+            const sampleComics = [24, 25, 26, 27, 28, 29, 22, 21, 20, 19, 18, 17];
+            return sampleComics[Math.floor(Math.random()*sampleComics.length)];
+        }
+
+    const url = `http://images.ucomics.com/comics/ga/2000/ga0007${getRandomComics()}.gif`;
+
+    return (
+    <img src={url}
+    alt="garfield"></img>);
 }
