6cba9c443fc53d4db1fb8e17cfa55bf6676d3544
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-24 10:56:24 +0200

    [add] Recipe component to Container

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,6 +3,7 @@ import { Button, ButtonGroup } from '@material-ui/core';
 import MainColumn1 from './main/MainColumn1';
 import MainColumn2 from './main/MainColumn2';
 import Comics from './main/Comics';
+import Recipe from './main/Recipe';
 
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const today  = new Date();
@@ -31,7 +32,7 @@ export default function Container() {
             <div className="Grid-item Main Main-column-2">
                 <MainColumn2></MainColumn2></div>
             <div className="Grid-item Main Main-column-3">MainColumn3</div>
-            <div className="Grid-item Main Main-column-4">MainColumn4</div>
+            <div className="Grid-item Main Main-column-4"><Recipe /></div>
             <div className="Grid-item Main-comic"><Comics /></div>
             <div className="Grid-item Main-bottom">MainBottom</div>
             <div className="Grid-item Footer">Footer</div>
