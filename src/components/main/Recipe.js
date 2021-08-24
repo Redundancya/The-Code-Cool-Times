6c9d22f1b9f6f3803e5f0b08ef0a0c50df62ac96
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-24 11:59:36 +0200

    [add] basic styling and list of ingredients

--- src/components/main/Recipe.js
+++ src/components/main/Recipe.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Button } from '@material-ui/core';
 
 export default function Recipe() {
 
@@ -16,11 +17,21 @@ export default function Recipe() {
 
 
     return (
-        <div>
+        <div width="150px" alignitems="center">
+            <h4>Today's meal:</h4> 
             <h3>{recipe.strMeal}</h3>
             <p>
-                <img src={recipe.strMealThumb} alt={recipe.strMeal} width="100%"></img>
+                <img src={recipe.strMealThumb} alt={recipe.strMeal} 
+                width="150px" height="auto"></img>
             </p>
+            <ul>
+                <li>{recipe.strIngredient1}</li>
+                <li>{recipe.strIngredient2}</li>
+                <li>{recipe.strIngredient3}</li>
+                <li>{recipe.strIngredient4}</li>
+                <li>{recipe.strIngredient5} ...</li>
+            </ul>
+            <Button aria-label="outlined primary button group">See recipe</Button>
         </div>
-        );
+        );  
 }
