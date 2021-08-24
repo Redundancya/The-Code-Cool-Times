26224cb7067c562ca6f533fdc83339a7f463726b
Redundancia <hunyadiwalter94@gmail.com>
2021-08-24 10:54:48 +0200

    [add] next/previous buttons for news

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,6 +1,7 @@
 import "../../App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ButtonGroup, Button } from "@material-ui/core";
 
 export const News = () => {
   const [articlesIndex, setArticlesIndex] = useState(0);
@@ -33,6 +34,28 @@ export const News = () => {
         <h3>{articles[articlesIndex]?.title ?? "Title loading..."}</h3>
       </a>
       <p>{articles[articlesIndex]?.description ?? "Content loading..."}</p>
+
+      <ButtonGroup size="small" aria-label="primary button group">
+        <Button
+          size="small"
+          style={{
+            fontSize: "10px",
+            fontFamily: "EB Garamond, sans-serif",
+          }}
+        >
+          Next
+        </Button>
+        <Button
+          size="small"
+          style={{
+            fontSize: "10px",
+            fontFamily: "EB Garamond, sans-serif",
+          }}
+          fontSize="8px"
+        >
+          Previous
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
