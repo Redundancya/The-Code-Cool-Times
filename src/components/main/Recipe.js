20d081368aa1642bf196f59f4e29886e5c3abed5
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-24 15:20:08 +0200

    [change] return according to state

--- src/components/main/Recipe.js
+++ src/components/main/Recipe.js
@@ -1,12 +1,18 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, setState } from 'react';
 import axios from 'axios';
 import { Button } from '@material-ui/core';
+import MainColumn1 from './MainColumn1';
 
 export default function Recipe() {
-
     const recipeUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
     
     const [recipe, setRecipe] = useState([]);
+    const [state, setState] = useState('start')
+
+    const changeState = () => {
+        setState(state === 'start' ? 'description' : 'start');
+        console.log(state)
+      }
 
     const getRecipeWithShortDescription = useCallback(() => {
             axios.get(recipeUrl)
@@ -33,20 +39,29 @@ export default function Recipe() {
         return ingredients.slice(0, maxNumber);
     }
 
-    return (
-        <div width="150px" alignitems="center">
-            <h4>Today's meal:</h4> 
-            <h3>{recipe.strMeal}</h3>
-            <p>
-                <img src={recipe.strMealThumb} alt={recipe.strMeal} 
-                width="150px" height="auto"></img>
-            </p>
-            <ul>
-            {getMaxNumOfIngredients().map(ingredient =>
-                <li>{ingredient}</li>)}
-                <li>...</li>
-            </ul>
-            <Button aria-label="outlined primary button group">See recipe</Button>
-        </div>
-        );  
+    if(state === 'start'){
+        return (
+        <div width="100%" alignitems="center">
+        <h4>Today's meal:</h4> 
+        <h3>{recipe.strMeal}</h3>
+        <p>
+            <img src={recipe.strMealThumb} alt={recipe.strMeal} 
+            width="150px" height="auto"></img>
+        </p>
+        <ul>
+        {getMaxNumOfIngredients().map(ingredient =>
+            <li>{ingredient}</li>)}
+            <li>...</li>
+        </ul>
+        <Button aria-label="outlined primary button group" onClick={() => changeState()}>See recipe</Button>
+    </div>
+        )
+    } else {
+        return (
+        <div width="100%" alignitems="center">
+            <h4>{recipe.strMeal} step by step description:</h4> 
+            <p width="100%">{recipe.strInstructions}</p>
+            <Button aria-label="outlined primary button group" onClick={() => changeState()}>Back</Button>
+        </div>)
+    }
 }
