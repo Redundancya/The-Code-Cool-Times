5d96c1e5f8787775144f5bc3064c80373dd64e2e
Redundancia <hunyadiwalter94@gmail.com>
2021-08-24 16:43:42 +0200

    [add] readd new render return

--- src/components/main/MainNews.js
+++ src/components/main/MainNews.js
@@ -5,18 +5,64 @@ import placeholder from "../../resources/images/placeholder.png";
 import { NewsContext } from "./NewsContext";
 
 export const MainNews = (props) => {
-  const [news, setNews] = useContext(NewsContext);
+  const [articles, setArticles] = useContext(NewsContext);
+  const [articleIndex, setArticleIndex] = useState(0);
 
   let newsIndex;
 
+  const changeNews = (incrementer) => {
+    if (incrementer === "next") {
+      setArticleIndex((articleIndex + 1) % 19);
+    } else {
+      setArticleIndex((articleIndex + 18) % 19);
+    }
+  };
+
   return (
     <div>
-      {console.log(news)}
-      {news.map((oneNews) => (
+      {console.log(articles)}
+      {articles.map((oneNews) => (
         <p>{oneNews.name}</p>
       ))}
     </div>
   );
+  //   <div>
+  //   <img
+  //     className="mainPicture"
+  //     src={articles[articlesIndex].urlToImage}
+  //     alt={articles[articlesIndex]?.source?.name ?? "Loading..."}
+  //   />
+  //   <a className="link" href={articles[articlesIndex]?.url ?? "Loading..."}>
+  //     <h3>{articles[articlesIndex]?.title ?? "Title loading..."}</h3>
+  //   </a>
+  //   <p>{articles[articlesIndex]?.description ?? "Content loading..."}</p>
+
+  //   <ButtonGroup size="small" aria-label="primary button group">
+  //     <Button
+  //       disabled={articlesIndex === 0 ? true : false}
+  //       onClick={() => changeNews("previous")}
+  //       size="small"
+  //       style={{
+  //         fontSize: "10px",
+  //         fontFamily: "EB Garamond, sans-serif",
+  //       }}
+  //     >
+  //       Previous
+  //     </Button>
+  //     <Button
+  //       disabled={articlesIndex === 19 ? true : false}
+  //       onClick={() => changeNews("next")}
+  //       size="small"
+  //       style={{
+  //         fontSize: "10px",
+  //         fontFamily: "EB Garamond, sans-serif",
+  //       }}
+  //     >
+  //       Next
+  //     </Button>
+  //   </ButtonGroup>
+  // </div>
+  // );
 };
 
 export default MainNews;
