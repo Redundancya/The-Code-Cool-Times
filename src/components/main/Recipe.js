2214a59ed13dc098fe5968fe942bb8a19b9c8e9b
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-24 15:56:48 +0200

    [refactor] return and change constants for ingredients and description

--- src/components/main/Recipe.js
+++ src/components/main/Recipe.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useCallback, setState } from 'react';
 import axios from 'axios';
 import { Button } from '@material-ui/core';
-import MainColumn1 from './MainColumn1';
 
 export default function Recipe() {
+
     const recipeUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
     
     const [recipe, setRecipe] = useState([]);
@@ -17,7 +17,7 @@ export default function Recipe() {
     const getRecipeWithShortDescription = useCallback(() => {
             axios.get(recipeUrl)
             .then(response => { 
-                if(response.data.meals[0].strInstructions.length > 500){
+                if(response.data.meals[0].strInstructions.length > 550){
                     getRecipeWithShortDescription();
                 } else {
                     setRecipe(response.data.meals[0]);
@@ -30,8 +30,9 @@ export default function Recipe() {
         getRecipeWithShortDescription();
         }, [getRecipeWithShortDescription])
 
+
     const getMaxNumOfIngredients = () => {
-        const maxNumber = 3;
+        const maxNumber = 4;
         let ingredients = Object.keys(recipe)
             .filter(item => item.toString().startsWith("strIngredient"))
             .filter(key => recipe[key] !== "")
@@ -39,29 +40,28 @@ export default function Recipe() {
         return ingredients.slice(0, maxNumber);
     }
 
-    if(state === 'start'){
-        return (
+    const recipeFront = (
         <div width="100%" alignitems="center">
-        <h4>Today's meal:</h4> 
-        <h3>{recipe.strMeal}</h3>
-        <p>
-            <img src={recipe.strMealThumb} alt={recipe.strMeal} 
-            width="150px" height="auto"></img>
-        </p>
-        <ul>
-        {getMaxNumOfIngredients().map(ingredient =>
-            <li>{ingredient}</li>)}
-            <li>...</li>
-        </ul>
-        <Button aria-label="outlined primary button group" onClick={() => changeState()}>See recipe</Button>
-    </div>
-        )
-    } else {
-        return (
+            <h4>Today's meal:</h4> 
+            <h3>{recipe.strMeal}</h3>
+            <p>
+                <img src={recipe.strMealThumb} alt={recipe.strMeal} 
+                width="150px" height="auto"></img>
+            </p>
+            <ul>
+            {getMaxNumOfIngredients().map(ingredient =>
+                <li>{ingredient}</li>)}
+            </ul>
+            <Button aria-label="outlined primary button group" onClick={() => changeState()}>See recipe</Button>
+        </div>)
+
+    const recipeBack = (
         <div width="100%" alignitems="center">
             <h4>{recipe.strMeal} step by step description:</h4> 
             <p width="100%">{recipe.strInstructions}</p>
             <Button aria-label="outlined primary button group" onClick={() => changeState()}>Back</Button>
         </div>)
-    }
+
+        return (state === 'start' ? recipeFront : recipeBack);
+
 }
