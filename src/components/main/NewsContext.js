cb7981b3afa548205db382c0e19163a64f68ddef
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-25 15:11:44 +0200

    [add] one more API key to bypass request limit

--- src/components/main/NewsContext.js
+++ src/components/main/NewsContext.js
@@ -3,15 +3,17 @@ import axios from "axios";
 
 export const NewsContext = createContext();
 
+// const TusisApi =
+//   "https://newsapi.org/v2/everything?qInTitle=everything&pageSize=20&apiKey=803b1f20229542109d3b21b58d162064";
+const RokysApi =
+  "https://newsapi.org/v2/everything?qInTitle=everything&pageSize=20&apiKey=d1f3e37a2d654d0dadc45046a0ab9ec7";
+
 export const NewsProvider = (props) => {
   const [loading, setLoading] = useState(true);
 
   const getTopNews = () => {
     axios
-      .get(
-        "https://newsapi.org/v2/everything?qInTitle=everything&pageSize=20&apiKey=803b1f20229542109d3b21b58d162064"
-      )
-
+      .get(RokysApi)
       .then((response) => {
         setArticles(response.data.articles);
         setLoading(false);
