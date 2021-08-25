2e325a2f2f255cd0cd3c418ceaba4930b35bf198
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-25 16:45:25 +0200

    [add] ThemeContext with state and toggleTheme

---
+++ src/ThemeContext.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState } from "react";
+
+const ThemeContext = new React.createContext();
+const ThemeUpdateContext = new React.createContext();
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
+
+export function useThemeUpdate() {
+  return useContext(ThemeUpdateContext);
+}
+
+export function ThemeProvider({ children }) {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  }
+
+  return (
+    <ThemeContext.Provider value={darkTheme}>
+      <ThemeUpdateContext.Provider value={toggleTheme}>
+        {children}
+      </ThemeUpdateContext.Provider>
+    </ThemeContext.Provider>
+  );
+}
