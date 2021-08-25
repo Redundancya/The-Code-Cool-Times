350775833c6b9f77af5d1ec52aa3386a550a709f
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-25 16:44:28 +0200

    [wrap] App.js in ThemeProvider context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Container from "./components/Container";
 import { NewsProvider } from "./components/main/NewsContext";
+import { ThemeProvider } from "./ThemeContext";
 
 export default function App() {
   return (
     <NewsProvider>
-      <div className="App">
-        <Container></Container>
-      </div>
+      <ThemeProvider>
+        <div className="App">
+          <Container></Container>
+        </div>
+      </ThemeProvider>
     </NewsProvider>
   );
 }
