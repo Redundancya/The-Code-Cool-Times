4f0961e93f87212dca614e092364d410d4528740
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-25 18:21:46 +0200

    [move] CSS rules to GlobalStyles.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import "./App.css";
 import Container from "./components/Container";
 import { NewsProvider } from "./components/main/NewsContext";
-import { ThemeProvider } from "./ThemeContext";
+import { ThemeProvider } from "./theme/ThemeContext";
+import { GlobalStyles } from "./theme/GlobalStyles";
 
 export default function App() {
   return (
-    <NewsProvider>
-      <ThemeProvider>
-        <div className="App">
-          <Container></Container>
-        </div>
-      </ThemeProvider>
-    </NewsProvider>
+    <>
+      <NewsProvider>
+        <ThemeProvider>
+          <GlobalStyles />
+          <div className="App">
+            <Container />
+          </div>
+        </ThemeProvider>
+      </NewsProvider>
+    </>
   );
 }
