8410acb1bcec21b8f7e62d7ad1eb25a4880c422d
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-26 22:01:18 +0200

    [add] api key selector in NewsContext

--- src/components/main/NewsContext.js
+++ src/components/main/NewsContext.js
@@ -3,10 +3,15 @@ import axios from "axios";
 
 export const NewsContext = createContext();
 
-const apiKey = "d1f3e37a2d654d0dadc45046a0ab9ec7"; // Roky's
-//const apiKey = "803b1f20229542109d3b21b58d162064"; // Tusi's
+const apiKeys = [
+  "803b1f20229542109d3b21b58d162064", // Tusi's
+  "d1f3e37a2d654d0dadc45046a0ab9ec7", // Roky's
+  "42bb7b3ce3b34a91aef6e8ff7ae74ec6", // Roky's 2nd
+  "e66d041687414a339b55f798c3fa8b6d", // Marta's
+];
+const apiKey = apiKeys[3];
 
-export const NewsProvider = ({ children }) => {
+export const NewsProvider = (props) => {
   const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
   const [newsTheme, setNewsTheme] = useState("everything");
