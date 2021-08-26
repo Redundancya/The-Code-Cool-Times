d5c8614656f2e68e69b58651736bf57a8404c50d
Redundancia <hunyadiwalter94@gmail.com>
2021-08-26 17:48:03 +0200

    [refactor] .then into async await, article state init

--- src/components/main/NewsContext.js
+++ src/components/main/NewsContext.js
@@ -1,9 +1,9 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
 export const NewsContext = createContext();
 
-const apiBaseUrl =
+const apiTopNewsUrl =
   "https://newsapi.org/v2/everything?q=everything&pageSize=20&apiKey=";
 
 // const apiKey = "d1f3e37a2d654d0dadc45046a0ab9ec7"; // Roky's
@@ -11,18 +11,20 @@ const apiKey = "803b1f20229542109d3b21b58d162064"; // Tusi's
 
 export const NewsProvider = (props) => {
   const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
 
-  const getTopNews = () => {
-    axios.get(`${apiBaseUrl}${apiKey}`).then((response) => {
-      const filteredResponse = response.data.articles.filter(
-        (article) => !article.description.includes("</")
-      );
-      setArticles(filteredResponse);
-      setLoading(false);
-    });
+  useEffect(() => {
+    getTopNews();
+  }, []);
+
+  const getTopNews = async () => {
+    const response = await axios.get(`${apiTopNewsUrl}${apiKey}`);
+    const responsesWithNoTag = response.data.articles.filter(
+      (article) => !article.description.includes("</")
+    );
+    setArticles(responsesWithNoTag);
+    setLoading(false);
   };
-
-  const [articles, setArticles] = useState(() => getTopNews());
 
   return (
     <NewsContext.Provider
