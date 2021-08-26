8121924d46fb5407c8ab1ed99f79207b69c04b78
Redundancia <hunyadiwalter94@gmail.com>
2021-08-26 20:12:14 +0200

    [refactor] change async url

--- src/components/main/NewsContext.js
+++ src/components/main/NewsContext.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 export const NewsContext = createContext();
 
-// const apiKey = "d1f3e37a2d654d0dadc45046a0ab9ec7"; // Roky's
-const apiKey = "803b1f20229542109d3b21b58d162064"; // Tusi's
+const apiKey = "d1f3e37a2d654d0dadc45046a0ab9ec7"; // Roky's
+//const apiKey = "803b1f20229542109d3b21b58d162064"; // Tusi's
 
 export const NewsProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
@@ -22,14 +22,12 @@ export const NewsProvider = ({ children }) => {
   const getTopNewsForTheme = async (newsTheme) => {
     try {
       const response = await axios.get(
-        "https://newsapi.org/v2/everything?q=" +
-          newsTheme +
-          "&apiKey=803b1f20229542109d3b21b58d162064"
+        `https://newsapi.org/v2/everything?q=${newsTheme}&apiKey=${apiKey}`
       );
       const responsesWithNoTag = response.data.articles.filter(
         (article) => !article.description.includes("</")
       );
-      setArticles(await responsesWithNoTag);
+      setArticles(responsesWithNoTag);
       setLoading(false);
     } catch (error) {
       console.log(Object.keys(error), error.message);
