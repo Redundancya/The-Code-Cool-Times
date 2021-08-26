ed409457460beca263879f4e6cb45b83584001a3
Redundancia <hunyadiwalter94@gmail.com>
2021-08-26 10:56:26 +0200

    [add] axios post for refactoring

--- src/components/main/Nameday.js
+++ src/components/main/Nameday.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Nameday = () => {
   const [nameday, setNameday] = useState([]);
 
   useEffect(() => {
+    //working version
     function performSignIn() {
       fetch("https://nameday.abalin.net/today", {
         mode: "cors",
@@ -17,8 +19,25 @@ export const Nameday = () => {
           console.log("Authorization failed : " + error.message)
         );
     }
+
+    //axios async await post
+    async function asyncPerformSignIn() {
+      const response = axios({
+        method: "post",
+        url: "https://nameday.abalin.net/today",
+        data: {},
+        headers: {
+          "Access-Control-Allow-Credentials": "true",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+          "Access-Control-Allow-Headers":
+            "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        }, // doesn't work atm
+      });
+      console.log(response);
+    }
     performSignIn();
   }, []);
 
-  return <div>{nameday}</div>;
+  return <div>Nameday: {nameday}</div>;
 };
