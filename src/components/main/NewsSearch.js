27d0371d56b8f2e23b240649e0f5febd821fc642
Redundancia <hunyadiwalter94@gmail.com>
2021-09-01 12:42:48 +0200

    [add] search for any theme

---
+++ src/components/main/NewsSearch.js
@@ -0,0 +1,59 @@
+import "../../App.css";
+import React, { useContext, useEffect, useState } from "react";
+import { NewsContext } from "./NewsContext";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      width: 120,
+      padding: 0,
+      marginLeft: theme.spacing(1),
+    },
+  },
+}));
+
+export default function NewsSearch() {
+  const context = useContext(NewsContext);
+  const classes = useStyles();
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const listener = (event) => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        event.preventDefault();
+        handleSubmit();
+      }
+    };
+    const handleSubmit = () => {
+      if (value) {
+        context.changeNewsTheme(value);
+        context.setLoading(true);
+      }
+
+      setValue("");
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [value]);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField
+        id="outlined-basic"
+        label="Other..."
+        variant="outlined"
+        size="small"
+        value={value}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
